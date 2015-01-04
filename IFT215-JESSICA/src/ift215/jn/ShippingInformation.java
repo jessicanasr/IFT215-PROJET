77d80536984f98d49d3c746c8a7d531dd92afd52
jessicanasr <j_nasr@live.com>
2015-01-04 13:32:49 +0200

    changes

---
+++ IFT215-JESSICA/src/ift215/jn/ShippingInformation.java
@@ -0,0 +1,73 @@
+
+package ift215.jn;
+
+
+public class ShippingInformation {
+    
+    private String name;
+    private int country;
+    private String address;
+    private boolean cash;
+    private boolean card;
+    private String shippingInformation;
+
+    
+    public ShippingInformation (String name, int country, String address){
+        
+        this.name=name; 
+        this.country=country;
+        this.address=address;
+        
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public void setName(String name){
+        this.name=name;
+    }
+    
+    public int getCountry(){
+        return country;
+    }
+    
+    public void setCountry(int country){
+        this.country=country;
+    }
+    
+    public String getAddress(){
+        return address;
+    }
+    
+    public void setAddress(String address){
+        this.address=address;
+    }
+    
+    public boolean getCash(){
+        return cash;
+    }
+    
+    public void setCash(boolean cash){
+        this.cash=cash;
+    }
+    
+    public boolean getCard(){
+        return card;
+    }
+    
+    public void setCard(boolean card){
+        this.card=card;
+    }
+    
+            
+    
+                
+    
+    
+    
+   
+       
+       
+
+}
